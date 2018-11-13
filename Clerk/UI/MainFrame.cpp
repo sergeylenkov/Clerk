a03b5e2e981d85258fe085faebb4cc233db63369
Sergey Lenkov <serglazy@gmail.com>
2018-11-13 09:08:07 +0300

    App icon fix

--- Clerk/UI/MainFrame.cpp
+++ Clerk/UI/MainFrame.cpp
@@ -7,7 +7,7 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
 
 	this->SetSize(wxSize(Settings::GetInstance().GetWindowWidth(), Settings::GetInstance().GetWindowHeight()));
 
-	this->SetIcon(wxICON(AppIcon));
+	this->SetIcon(wxICON(APP_ICON));
 
 	wxSplitterWindow *splitter = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOBORDER);
 	splitter->SetSashGravity(0.5);

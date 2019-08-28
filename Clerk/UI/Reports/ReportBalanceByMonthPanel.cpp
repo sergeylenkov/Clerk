2c2272df58a7a0ae37f852630884700ef0bcbef2
Sergey Lenkov <serglazy@gmail.com>
2019-08-28 21:17:29 +0300

    Small refactoring

--- Clerk/UI/Reports/ReportBalanceByMonthPanel.cpp
+++ Clerk/UI/Reports/ReportBalanceByMonthPanel.cpp
@@ -213,13 +213,7 @@ void ReportBalancePanel::CalculatePeriod() {
 		break;
 
 	case 3:
-		fromDate.Subtract(wxDateSpan::wxDateSpan(1, 0, 0, 0));
-		fromDate.SetMonth(wxDateTime::Month::Jan);
-		fromDate.SetDay(1);
-
-		toDate.Subtract(wxDateSpan::wxDateSpan(1, 0, 0, 0));
-		toDate.SetMonth(wxDateTime::Month::Dec);
-		toDate.SetToLastMonthDay(wxDateTime::Month::Dec);
+		Utils::CalculatePeriod(PeriodTypes::PreviousYear, fromDate, toDate);
 		break;
 
 	case 4:
--- Clerk/UI/Reports/ReportExpensesByMonthPanel.cpp
+++ Clerk/UI/Reports/ReportExpensesByMonthPanel.cpp
@@ -227,13 +227,7 @@ void ReportExpensesByMonthPanel::CalculatePeriod() {
 			break;
 
 		case 3:
-			fromDate.Subtract(wxDateSpan::wxDateSpan(1, 0, 0, 0));
-			fromDate.SetMonth(wxDateTime::Month::Jan);
-			fromDate.SetDay(1);
-
-			toDate.Subtract(wxDateSpan::wxDateSpan(1, 0, 0, 0));
-			toDate.SetMonth(wxDateTime::Month::Dec);
-			toDate.SetToLastMonthDay(wxDateTime::Month::Dec);
+			Utils::CalculatePeriod(PeriodTypes::PreviousYear, fromDate, toDate);
 			break;
 
 		case 4:
